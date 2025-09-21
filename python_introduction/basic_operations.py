77ec2f3755c8c2ee77cf35f5cc9fcee535eb9bff
Karabo <penny.matsebe@gmail.com>
2025-09-21 17:08:07 +0100

    Add basic operatiosn calculations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+
+print(f"The sum of {number1} and {number2} is {sum}")
+print(f"The difference between {number1} and {number2} is {difference}")
+print(f"The product of {number1} and {number2} is {product}")
